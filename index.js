25e3e6d331c4f1327364c75c9bae2ec77375800b
KliazovichAlex <67436594+KliazovichAlex@users.noreply.github.com>
2020-08-06 02:02:39 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,5 +6,51 @@
 // admin = name;
 // alert(admin);
 
-let userName = prompt("Введите ваше имя.");
-alert("Добро пожаловать " + userName + "!");
+// let userName = prompt("Введите ваше имя.");
+// alert("Добро пожаловать " + userName + "!");
+
+let userName = prompt("Введите ваше имя");
+let userMidlename = prompt("Введите ваше отчество");
+let userLastname = prompt("Введите вашу фамилию");
+let userAge = prompt("Введите ваш возрост (в годах)");
+let userSex = confirm("Ваш пол мужской?");
+let userAgeAfter = Number(+userAge + 5);
+let isUserOld = null;
+if (userSex === true) {
+  userSex = "Мужской";
+} else {
+  userSex = "Женский";
+}
+if (userSex === true && userAge >= 65) {
+  isUserOld = "Да";
+} else {
+  isUserOld = " Нет";
+}
+
+// if (userSex === false && userAge >= 55) {
+//   isUserOld = "Да";
+// } else {
+//   isUserOld = " Нет";
+// }
+
+alert(
+  "Ваше ФИО :" +
+    userName +
+    userMidlename +
+    userLastname +
+    "\n" +
+    "Ваш возрост в годах : " +
+    userAge +
+    "\n" +
+    "ваш возрост в днях : " +
+    userAge * 365 +
+    "\n" +
+    "Через 5 лет вам будет : " +
+    userAgeAfter +
+    "\n" +
+    "Ваш пол:  " +
+    userSex +
+    "\n" +
+    " Вы на пенсии : " +
+    isUserOld
+);
