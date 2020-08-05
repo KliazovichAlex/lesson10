e0403da74644a70945d25bc890cf9ac5458bd0c7
Kliazovich Alex <aliaksandrkliazovich@gamil.com>
2020-08-05 21:38:54 +0300

    part of homework

---
+++ index.js
@@ -0,0 +1,10 @@
+// alert("Я JavaScript!");  Задание 2.1
+
+// let admin;
+// let name;
+// name = "Джон";
+// admin = name;
+// alert(admin);
+
+let userName = prompt("Введите ваше имя.");
+alert("Добро пожаловать " + userName + "!");
